b99c2557f89074216798465fd4a4e9d24d2b3389
Karen Cárcamo <karencfv@users.noreply.github.com>
2020-05-12 13:02:06 +1200

    Update ecctl version to 1.0.0-beta3 (#39)

--- Formula/ecctl.rb
+++ Formula/ecctl.rb
@@ -2,14 +2,14 @@
 class Ecctl < Formula
   desc "Elastic Cloud Control, the official Elastic Cloud and ECE command line interface"
   homepage "https://github.com/elastic/ecctl"
-  version "1.0.0-beta2"
+  version "1.0.0-beta3"
 
   if OS.mac?
-    url "https://download.elastic.co/downloads/ecctl/1.0.0-beta2/ecctl_1.0.0-beta2_darwin_amd64.tar.gz", :using => CurlDownloadStrategy
-    sha256 "b782b574ee1aa8bd63c2e81769f9236bbf144a19ed8a391994fa0a999ec9a9a8"
+    url "https://download.elastic.co/downloads/ecctl/1.0.0-beta3/ecctl_1.0.0-beta3_darwin_amd64.tar.gz", :using => CurlDownloadStrategy
+    sha256 "2c2a6d3c64d93f3ef7edb6d8dfa4ce279770faad20b6d9c31cb8a3319dbbc3ef"
   elsif OS.linux?
-    url "https://download.elastic.co/downloads/ecctl/1.0.0-beta2/ecctl_1.0.0-beta2_linux_amd64.tar.gz", :using => CurlDownloadStrategy
-    sha256 "753462453ca1a9b4ff436947b1d0f3cf67f986629f5b904dcdfc6361a12f89b0"
+    url "https://download.elastic.co/downloads/ecctl/1.0.0-beta3/ecctl_1.0.0-beta3_linux_amd64.tar.gz", :using => CurlDownloadStrategy
+    sha256 "5c05139a9fd61341efc9ff9e9b9623341fd8005f8581be8a74951ac8a296726a"
   end
 
   def install
