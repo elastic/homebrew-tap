67bdddb40289c7471b2cdc221ae8e320a7823961
Chris <chris.koehnke@elastic.co>
2021-08-16 13:12:44 -0400

    Fix ci homebrew installation (#97)
    Source the homebrew setup script to correctly set the environment
    variables for other scripts.

--- .ci/release.sh
+++ .ci/release.sh
@@ -5,7 +5,7 @@ set -eou pipefail
 DOWNLOAD_BASE="$1"
 VERSION="$2"
 
-.ci/prepare-homebrew-env.sh
+source .ci/prepare-homebrew-env.sh
 
 # update to the new artifacts
 env DOWNLOAD_BASE="$DOWNLOAD_BASE" .ci/update.sh "$VERSION"
